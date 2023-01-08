8704c0a9ac8eb5489240320db4bcd536947b31bf
PrasanthKumar Yernagula <86708678+Prashanthkumarpk@users.noreply.github.com>
2023-01-08 22:29:49 +0530

    june

--- assets/js/comingsoon.js
+++ assets/js/comingsoon.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("May, 2023 00:00:00").getTime();
+    const countDate = new Date("June, 2023 00:00:00").getTime();
 
     const currentTime = new Date().getTime();
     console.log(currentTime);
